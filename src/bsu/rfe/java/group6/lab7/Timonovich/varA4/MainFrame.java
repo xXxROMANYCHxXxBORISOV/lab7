6eba7d723ed3e5d94f913641319d0a12a65a5863
Romanych <yurytimon21@gmail.com>
2021-05-02 17:21:21 +0300

    first commit

---
+++ src/bsu/rfe/java/group6/lab7/Timonovich/varA4/MainFrame.java
@@ -0,0 +1,33 @@
+package bsu.rfe.java.group6.lab7.Timonovich.varA4;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+
+public class MainFrame {
+}
